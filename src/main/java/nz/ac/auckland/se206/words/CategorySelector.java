efa7f95d309251cda9164da71296f1cec29526c5
Jeffery <jcao926@aucklanduni.ac.nz>
2022-09-07 20:43:26 +1200

    Added setWordList method to create a list of words the user has not encountered to give to the user for every new game.

--- src/main/java/nz/ac/auckland/se206/words/CategorySelector.java
+++ src/main/java/nz/ac/auckland/se206/words/CategorySelector.java
@@ -1,7 +1,5 @@
 package nz.ac.auckland.se206.words;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,51 +11,55 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 public class CategorySelector {
 
-  public enum Difficulty {
-    E,
-    M,
-    H
-  }
+	public enum Difficulty {
+		E, M, H
+	}
 
-  private Map<Difficulty, List<String>> difficultyToCategories;
+	private Map<Difficulty, List<String>> difficultyToCategories;
 
-  public CategorySelector() throws IOException, CsvException, URISyntaxException {
-    difficultyToCategories = new HashMap<>();
-    for (Difficulty difficulty : Difficulty.values()) {
-      // creates an empty ArrayList for each difficulty
-      difficultyToCategories.put(difficulty, new ArrayList<>());
-    }
+	public CategorySelector() throws IOException, CsvException, URISyntaxException {
+		difficultyToCategories = new HashMap<>();
+		for (Difficulty difficulty : Difficulty.values()) {
+			// creates an empty ArrayList for each difficulty
+			difficultyToCategories.put(difficulty, new ArrayList<>());
+		}
 
-    for (String[] line : getLines()) {
-      // adds each word on each line to its corresponding difficulty ArrayList
-      difficultyToCategories.get(Difficulty.valueOf(line[1])).add(line[0]);
-    }
-  }
+		for (String[] line : getLines()) {
+			// adds each word on each line to its corresponding difficulty ArrayList
+			difficultyToCategories.get(Difficulty.valueOf(line[1])).add(line[0]);
+		}
+	}
 
-  /**
-   * This method gets a random word of a specified difficulty
-   *
-   * @param difficulty difficulty of words to select from
-   * @return
-   */
-  public String getRandomCategory(Difficulty difficulty) {
-    // returns a random category of a specified difficulty
-    return difficultyToCategories
-        .get(difficulty)
-        .get(new Random().nextInt(difficultyToCategories.get(difficulty).size()));
-  }
+	/**
+	 * This method gets a random word of a specified difficulty
+	 *
+	 * @param difficulty difficulty of words to select from
+	 * @return
+	 */
+	public String getRandomCategory(Difficulty difficulty) {
+		// returns a random category of a specified difficulty
+		return difficultyToCategories.get(difficulty)
+				.get(new Random().nextInt(difficultyToCategories.get(difficulty).size()));
+	}
 
-  protected List<String[]> getLines() throws IOException, CsvException, URISyntaxException {
-    File fileName =
-        new File(CategorySelector.class.getResource("/category_difficulty.csv").toURI());
+	protected List<String[]> getLines() throws IOException, CsvException, URISyntaxException {
+		File fileName = new File(CategorySelector.class.getResource("/category_difficulty.csv").toURI());
 
-    try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8);
-        CSVReader reader = new CSVReader(fr)) {
-      // returns a list of string array where each string array is each line in the csv file
-      // each word in each string array is separated by commas in each line in the csv file
-      return reader.readAll();
-    }
-  }
+		try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8); CSVReader reader = new CSVReader(fr)) {
+			// returns a list of string array where each string array is each line in the
+			// csv file
+			// each word in each string array is separated by commas in each line in the csv
+			// file
+			return reader.readAll();
+		}
+	}
+
+	public List<String> getWordList(Difficulty difficulty) {
+		return difficultyToCategories.get(difficulty);
+	}
 }
