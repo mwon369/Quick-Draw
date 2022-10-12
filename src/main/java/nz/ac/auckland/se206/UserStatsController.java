c5bc7c193857457651713e4718dab7c04f7b80b1
Mitchell <mitchellwong833@gmail.com>
2022-10-13 02:28:30 +1300

    Added helper methods to keep the pie chart colours constant

--- src/main/java/nz/ac/auckland/se206/UserStatsController.java
+++ src/main/java/nz/ac/auckland/se206/UserStatsController.java
@@ -2,15 +2,19 @@ package nz.ac.auckland.se206;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class UserStatsController {
@@ -19,10 +23,14 @@ public class UserStatsController {
   @FXML private PieChart statsPieChart;
   @FXML private Label noGamesLabel;
 
+  private User currentUser;
+
+  private final String[] colours = {"orange", "#FFA071"};
+
   /** This method retrieves and displays the user's profile statistics */
   protected void onRetrieveStats() {
     // get logged in user
-    User currentUser = UsersManager.getSelectedUser();
+    currentUser = UsersManager.getSelectedUser();
     userLabel.setText(currentUser.getUsername());
 
     // don't show pie chart if user has no games played
@@ -32,7 +40,7 @@ public class UserStatsController {
       return;
     }
 
-    // show pie chart
+    // set pie chart data and make it visible
     noGamesLabel.setVisible(false);
     statsPieChart.setVisible(true);
     ObservableList<PieChart.Data> pieChartData =
@@ -41,6 +49,9 @@ public class UserStatsController {
             new PieChart.Data("Losses", currentUser.getLosses()));
 
     statsPieChart.setData(pieChartData);
+    colourPieChart(pieChartData);
+    changeLegendLabels();
+    changeLegendSymbols();
 
     // show individual numbers for each wedge
     statsPieChart
@@ -95,5 +106,65 @@ public class UserStatsController {
   @FXML
   private void onButtonHover() {
     SoundManager.playButtonHover();
+  }
+
+  /**
+   * This method colours the pie chart with custom colours whenever we load the user stats page.
+   * This is to prevent the colours from alternating when you continuously reload the user stats
+   * page
+   *
+   * @param pieChartData a list of the pie chart data
+   */
+  private void colourPieChart(ObservableList<PieChart.Data> pieChartData) {
+    int i = 0;
+    for (PieChart.Data data : pieChartData) {
+      data.getNode().setStyle("-fx-pie-color: " + colours[i % colours.length] + ";");
+      i++;
+    }
+  }
+
+  /**
+   * This method changes the colours of the pie chart legend symbols so that they are matching with
+   * the colours of the pie chart itself
+   */
+  private void changeLegendSymbols() {
+    // get the set of all labels in the pie chart legend
+    Set<Node> legendLabels = statsPieChart.lookupAll("Label.chart-legend-item");
+    int i = 0;
+
+    // iterate through the labels
+    for (Node legendLabel : legendLabels) {
+      Label label = (Label) legendLabel;
+      Circle circle;
+      // if we're iterating through the first label,
+      // change the colour of the label symbol to orange
+      if (i == 0) {
+        circle = new Circle(5.0, Color.ORANGE);
+      } else {
+        // otherwise change the label symbol colour to salmon
+        circle = new Circle(5.0, Color.SALMON);
+      }
+      label.setGraphic(circle);
+      i++;
+    }
+  }
+
+  /**
+   * This method changes the text of the chart legend labels so that they display the number of wins
+   * and losses the user has.
+   */
+  private void changeLegendLabels() {
+    // get the set of all labels in the pie chart legend
+    Set<Node> legendLabels = statsPieChart.lookupAll("Label.chart-legend-item");
+    int i = 0;
+    // iterate through the labels
+    for (Node legendLabel : legendLabels) {
+      Label label = (Label) legendLabel;
+      // if we're iterating through the first label, set the text for wins
+      // otherwise set the text for losses
+      label.setText(
+          i == 0 ? "Wins: " + currentUser.getWins() : "Losses: " + currentUser.getLosses());
+      i++;
+    }
   }
 }
