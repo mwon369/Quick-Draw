e833e4e557ad4900503d3f57dd10a8564ecd77b5
Jordan Wen Jie See <wsee046@aucklanduni.ac.nz>
2022-10-16 16:59:50 +1300

    Changed from pane to vboxes in badge view to center badge image

--- src/main/java/nz/ac/auckland/se206/BadgeViewController.java
+++ src/main/java/nz/ac/auckland/se206/BadgeViewController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,7 +21,7 @@ import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javax.imageio.ImageIO;
 import nz.ac.auckland.se206.badges.Badge;
@@ -37,21 +38,21 @@ public class BadgeViewController {
   @FXML private ImageView badgeIconEight;
   @FXML private ImageView badgeIconNine;
   @FXML private ImageView badgeIconTen;
-  @FXML private Pane badgeOnePane;
-  @FXML private Pane badgeTwoPane;
-  @FXML private Pane badgeThreePane;
-  @FXML private Pane badgeFourPane;
-  @FXML private Pane badgeFivePane;
-  @FXML private Pane badgeSixPane;
-  @FXML private Pane badgeSevenPane;
-  @FXML private Pane badgeEightPane;
-  @FXML private Pane badgeNinePane;
-  @FXML private Pane badgeTenPane;
+  @FXML private VBox badgeOneVbox;
+  @FXML private VBox badgeTwoVbox;
+  @FXML private VBox badgeThreeVbox;
+  @FXML private VBox badgeFourVbox;
+  @FXML private VBox badgeFiveVbox;
+  @FXML private VBox badgeSixVbox;
+  @FXML private VBox badgeSevenVbox;
+  @FXML private VBox badgeEightVbox;
+  @FXML private VBox badgeNineVbox;
+  @FXML private VBox badgeTenVbox;
 
   @FXML private Label badgeTitleLabel;
   @FXML private Label badgeDescriptionLabel;
   private List<ImageView> badgeIcons;
-  private List<Pane> badgePanes;
+  private List<VBox> badgeVboxes;
   private ArrayList<Badge> badgeList;
 
   /**
@@ -74,18 +75,18 @@ public class BadgeViewController {
             badgeIconEight,
             badgeIconNine,
             badgeIconTen);
-    badgePanes =
+    badgeVboxes =
         Arrays.asList(
-            badgeOnePane,
-            badgeTwoPane,
-            badgeThreePane,
-            badgeFourPane,
-            badgeFivePane,
-            badgeSixPane,
-            badgeSevenPane,
-            badgeEightPane,
-            badgeNinePane,
-            badgeTenPane);
+            badgeOneVbox,
+            badgeTwoVbox,
+            badgeThreeVbox,
+            badgeFourVbox,
+            badgeFiveVbox,
+            badgeSixVbox,
+            badgeSevenVbox,
+            badgeEightVbox,
+            badgeNineVbox,
+            badgeTenVbox);
     // Setup badge GUI for display
     badgeTitleLabel.setVisible(false);
     badgeDescriptionLabel.setVisible(false);
@@ -128,6 +129,8 @@ public class BadgeViewController {
       } else {
         badgeIcons.get(i).setImage(loadImage("questionicon"));
       }
+      badgeIcons.get(i).setFitHeight(100);
+      badgeIcons.get(i).setFitWidth(100);
     }
   }
 
@@ -149,7 +152,13 @@ public class BadgeViewController {
   }
 
   /** This method displays the badge information when a badge is selected */
-  private void showInformation(int badgeId, String paneName) {
+  /**
+   * This method displays the badge information when a badge is selected. Also borders the badge
+   *
+   * @param badgeId the badge to show information for
+   * @param vboxName the id of the badge vbox to border
+   */
+  private void showInformation(int badgeId, String vboxName) {
     // Displaying the badge's info to user
     badgeTitleLabel.setText(badgeList.get(badgeId).getTitle());
     badgeDescriptionLabel.setText(badgeList.get(badgeId).getDescription());
@@ -157,92 +166,92 @@ public class BadgeViewController {
     badgeDescriptionLabel.setVisible(true);
     badgeDescriptionLabel.setWrapText(true);
     // Highlighting the current badge selected
-    this.borderCurrentTool(paneName);
+    this.borderCurrentTool(vboxName);
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeOneSelect() {
     SoundManager.playButtonClick();
-    showInformation(0, "badgeOnePane");
+    showInformation(0, "badgeOneVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeTwoSelect() {
     SoundManager.playButtonClick();
-    showInformation(1, "badgeTwoPane");
+    showInformation(1, "badgeTwoVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeThreeSelect() {
     SoundManager.playButtonClick();
-    showInformation(2, "badgeThreePane");
+    showInformation(2, "badgeThreeVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeFourSelect() {
     SoundManager.playButtonClick();
-    showInformation(3, "badgeFourPane");
+    showInformation(3, "badgeFourVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeFiveSelect() {
     SoundManager.playButtonClick();
-    showInformation(4, "badgeFivePane");
+    showInformation(4, "badgeFiveVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeSixSelect() {
     SoundManager.playButtonClick();
-    showInformation(5, "badgeSixPane");
+    showInformation(5, "badgeSixVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeSevenSelect() {
     SoundManager.playButtonClick();
-    showInformation(6, "badgeSevenPane");
+    showInformation(6, "badgeSevenVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeEightSelect() {
     SoundManager.playButtonClick();
-    showInformation(7, "badgeEightPane");
+    showInformation(7, "badgeEightVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeNineSelect() {
     SoundManager.playButtonClick();
-    showInformation(8, "badgeNinePane");
+    showInformation(8, "badgeNineVbox");
   }
 
   /** This method plays the button click sound and also displays the badge description */
   @FXML
   private void onBadgeTenSelect() {
     SoundManager.playButtonClick();
-    showInformation(9, "badgeTenPane");
+    showInformation(9, "badgeTenVbox");
   }
 
   /**
    * This method adds a border to the currently selected badge so the user knows which badge is
    * currently selected
    *
-   * @param badgePaneId the pane Id of the badge
+   * @param badgeVboxId the vbox Id of the badge
    */
-  private void borderCurrentTool(String badgePaneId) {
+  private void borderCurrentTool(String badgeVboxId) {
     // for each tool pane, check if its id is equal to the specific tool pane id
-    for (Pane badgePane : badgePanes) {
+    for (VBox badgeVbox : badgeVboxes) {
       // if id is equal, change the color to a specific color, otherwise, change to
       // transparent
-      badgePane.setBorder(
-          badgePane.getId().equals(badgePaneId)
+      badgeVbox.setBorder(
+          badgeVbox.getId().equals(badgeVboxId)
               ? new Border(
                   new BorderStroke(
                       Color.rgb(226, 159, 0),
@@ -251,6 +260,7 @@ public class BadgeViewController {
                       new BorderWidths(4)))
               : new Border(
                   new BorderStroke(Color.TRANSPARENT, BorderStrokeStyle.SOLID, null, null)));
+      badgeVbox.setPadding(new Insets(0, -4, 0, -4));
     }
   }
 
